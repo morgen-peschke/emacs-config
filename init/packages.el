d1edafe5b19691136108836f6afe85ce679e8fd8
morgen-peschke <morgen.peschke@gmail.com>
2014-08-02 13:36:31 -0400

    Move lisp files out of main directory Fix cask directory Comment out el-get install of nxhtml New code to init nxhtml

--- init/packages.el
+++ init/packages.el
@@ -1,28 +1,28 @@
 ;;|====================================
 ;;| el-get install/load
 ;;|
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-
-(setq mpeschke/packages/el-get-package-list
-      '(nxhtml
-        ))
-
-(el-get 'sync mpeschke/packages/el-get-package-list)
+;(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;
+;(unless (require 'el-get nil 'noerror)
+;  (with-current-buffer
+;      (url-retrieve-synchronously
+;       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;    (goto-char (point-max))
+;    (eval-print-last-sexp)))
+;
+;(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;
+;(setq mpeschke/packages/el-get-package-list
+;      '(nxhtml
+;        ))
+;
+;(el-get 'sync mpeschke/packages/el-get-package-list)
 
 ;;|====================================
 ;;| Cask package management
 ;;|
 
-(require 'cask "/usr/local/Cellar/cask/0.5.2/cask.el")
+(require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 
@@ -31,7 +31,8 @@
 ;;|
 
 ;; add directories to load path
-(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(load "nxhtml/autostart.el")
 
 ;; Improved automatic alignment
 (load "align-string.el")
